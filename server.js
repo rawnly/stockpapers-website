867143f870812dee476c099e7d592ec217695479
Federico Vitale <fedevitale99@gmail.com>
2019-01-20 18:07:30 +0100

    new route

--- server.js
+++ server.js
@@ -1,27 +1,26 @@
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-const express = require('express')
-const next = require('next')
-const isDev = process.env.NODE_ENV !== "production"
+const express = require("express");
+const next = require("next");
+const isDev = process.env.NODE_ENV !== "production";
 
 const app = next({
-	dev: isDev
-})
+	dev: isDev,
+});
 
 app
 	.prepare()
 	.then(() => {
 		const server = express();
-		const handler = app.getRequestHandler()
+		const handler = app.getRequestHandler();
 
-		server.get('/itunes|store|download', (req, res) => {
-			res.redirect('http://get.stockpapers.app').status(301)
-		})
+		server.get("/itunes|store|download", (req, res) => res.redirect("http://get.stockpapers.app").status(301));
+		server.get("/author", (req, res) => res.redirect("https://rawnly.com").status(301));
 
-		server.get('*', (req, res) => handler(req, res))
+		server.get("*", (req, res) => handler(req, res));
 
-		server.listen(PORT, () => console.log(`Server running at: http://localhost:${PORT}`))
+		server.listen(PORT, () => console.log(`Server running at: http://localhost:${PORT}`));
 	})
-	.catch(error => {
-		console.error(`Error: Can't restart the server\n`, error)
-	})
+	.catch((error) => {
+		console.error(`Error: Can't restart the server\n`, error);
+	});
